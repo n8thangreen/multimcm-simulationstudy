7f9a9a9c45e05283c2ae4bebbbc300b883f86a35
Dr Nathan Green <n8thangreen@yahoo.co.uk>
2024-08-13 13:00:40 +0100

    completed looping over precompiled model

---
+++ R/run_scenario.R
@@ -0,0 +1,51 @@
+
+# obtain posterior samples for a single simulated data set
+#
+run_scenario <- function(x, sim_params, bmcm_params) {
+  set.seed(1234)
+  
+  # sample data set
+  dat <- do.call(rsurv_cf, sim_params)
+  
+  # background hazard
+  dat$rate <- 10^(-10)
+  
+  # hack because errors with only one treatment
+  dat <- 
+    mutate(dat, tx = 1) |> 
+    rbind(mutate(dat, tx = 2))
+  
+  bmcm_input <- c(input_data = list(dat), bmcm_params)
+  
+  # fit model
+  fit <- do.call(bmcm_stan, bmcm_input)
+  
+  samples <- extract_params(fit$output, pattern = "^(median|rmst|cf)") 
+  
+  # save to file
+  path_name <- paste0("samples_", x, ".csv")
+  write.csv(samples, file = path_name)
+  
+  return()
+}
+
+#
+extract_params <- function(fit, pattern) {
+  if (inherits(fit, "stanfit")) {
+    
+    samples <- rstan::extract(fit)
+    param_names <- grep(pattern, names(samples), value = TRUE)
+    extracted_params <- samples[param_names]
+    
+  } else if (inherits(fit, "CmdStanMCMC")) {
+    
+    samples <- fit$draws(format = "df")
+    param_names <- grep(pattern, names(samples), value = TRUE)
+    extracted_params <- samples[, param_names]
+  } else {
+    stop("Fit object must be of class 'stanfit' (rstan) or 'CmdStanMCMC' (cmdstanr).")
+  }
+  
+  extracted_params
+}
+
--- scripts/main_cmdstanr.R
+++ scripts/main_cmdstanr.R
@@ -10,64 +10,13 @@ library(glue)
 library(ggplot2)
 
 
-# obtain posterior samples for a single simulated data set
-#
-run_scenario <- function(x, sim_params, bmcm_params) {
-  set.seed(1234)
-  
-  # sample data set
-  dat <- do.call(rsurv_cf, sim_params)
-  
-  # background hazard
-  dat$rate <- 10^(-10)
-  
-  # hack because errors with only one treatment
-  dat <- 
-    mutate(dat, tx = 1) |> 
-    rbind(mutate(dat, tx = 2))
-  
-  bmcm_input <- c(input_data = list(dat), bmcm_params)
-  
-  # fit model
-  fit <- do.call(bmcm_stan, bmcm_input)
-  
-  samples <- extract_params(fit$output, pattern = "^(median|rmst|cf)") 
-  
-  browser()
-  # save to file
-  path_name <- paste0("samples_", x, ".csv")
-  write.csv(samples, file = path_name)
-  
-  invisible(samples)
-}
-
-#
-extract_params <- function(fit, pattern) {
-  if (inherits(fit, "stanfit")) {
-    
-    samples <- rstan::extract(fit)
-    param_names <- grep(pattern, names(samples), value = TRUE)
-    extracted_params <- samples[param_names]
-    
-  } else if (inherits(fit, "CmdStanMCMC")) {
-    
-    samples <- fit$draws(format = "df")
-    param_names <- grep(pattern, names(samples), value = TRUE)
-    extracted_params <- samples[, param_names]
-  } else {
-    stop("Fit object must be of class 'stanfit' (rstan) or 'CmdStanMCMC' (cmdstanr).")
-  }
-  
-  extracted_params
-}
-
 # read in scenario data
 scenario_data <- read.csv(here::here("raw-data/scenarios.csv")) |> as_tibble()
 
 i <- 1
 data <- scenario_data[i, ]
 latent_params_true <- eval(parse(text = data$latent_params_true))
-n_sim <- 10
+n_sim <- 2
 
 sim_params <-
   list(
@@ -101,6 +50,7 @@ bmcm_params <-
     use_cmdstanr = TRUE)
 
 # example simulated data
+# for structure
 dummy_sample <- do.call(rsurv_cf, sim_params)
 dummy_sample$rate <- 10^(-10)
 dummy_sample <- 
@@ -108,11 +58,12 @@ dummy_sample <-
   rbind(mutate(dummy_sample, tx = 2))
 
 stan_model <- 
-  precompile_bmcm_model(input_data = dummy_sample,
-                        family_latent = bmcm_params$family_latent,
-                        cureformula = bmcm_params$cureformula,
-                        use_cmdstanr = TRUE,
-                        file_path = here::here("stan"))
+  precompile_bmcm_model(
+    input_data = dummy_sample,
+    family_latent = bmcm_params$family_latent,
+    cureformula = bmcm_params$cureformula,
+    use_cmdstanr = TRUE,
+    file_path = here::here("stan"))
 
 bmcm_params$precompiled_model_path <- 
   here::here("stan", glue::glue("{stan_model$model_name()}.exe"))
