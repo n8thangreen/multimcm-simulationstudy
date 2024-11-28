a9fc1be72023c9315fdac058a2d932695736bf79
Dr Nathan Green <n8thangreen@yahoo.co.uk>
2024-11-28 13:42:25 +0000

    update mean summary tables

--- scripts/tables.R
+++ scripts/tables.R
@@ -32,6 +32,8 @@ scenario_mean_table <- function(data_list) {
 
 
 library(kableExtra)
+library(purrr)
+library(dplyr)
 
 # load data
 
@@ -51,7 +53,7 @@ load(glue::glue("data/summary_data_cluster_{model_type}.RData"))
 pm_hier <- pm
 summary_dat_hier <- summary_dat
 
-# both model types in a single table
+## both model types in a single table
 
 rmst_tab_sep <- 
   map(pm_sep, "rmst") |> 
@@ -70,27 +72,33 @@ cf_tab_hier <-
   scenario_mean_table()
 
 combined_cf_tab <- 
-  merge(cf_tab_sep, cf_tab_hier, by = "Scenario", suffixes = c(".sep", ".hier")) |> 
-  mutate(across(where(is.numeric), ~ round(., 2)))
-
-combined_cf_tab <- 
-  combined_tab[, order(names(combined_tab))] |> 
+  merge(cf_tab_sep, cf_tab_hier,
+        by = "Scenario",
+        suffixes = c(".sep", ".hier")) |> 
+  mutate(across(where(is.numeric), ~ round(., 2))) %>%
+  select(order(colnames(.))) |> 
   select(Scenario, everything())
 
 write.csv(combined_cf_tab, file = glue::glue("output_data/cf_table.csv"), row.names = FALSE)
 
 combined_rmst_tab <- 
-  merge(rmst_tab_sep, rmst_tab_hier, by = "Scenario", suffixes = c(".sep", ".hier")) |> 
-  mutate(across(where(is.numeric), ~ round(., 2)))
-
-combined_rmst_tab <- 
-  combined_tab[, order(names(combined_tab))] |> 
+  merge(rmst_tab_sep, rmst_tab_hier,
+        by = "Scenario",
+        suffixes = c(".sep", ".hier")) |> 
+  mutate(across(where(is.numeric), ~ round(., 2))) %>%
+  select(order(colnames(.))) |> 
   select(Scenario, everything())
 
 write.csv(combined_rmst_tab, file = glue::glue("output_data/rmst_table.csv"), row.names = FALSE)
 
+## single table
 
-# single table per model type
+combined_tab_all <- 
+  merge(combined_rmst_tab, combined_cf_tab, by = "Scenario")
+
+write.csv(combined_tab_all, file = glue::glue("output_data/combined_performance_table.csv"), row.names = FALSE)
+
+## single table per model type
 
 rmst_tab <- 
   map(pm, "rmst") |> 
@@ -100,27 +108,38 @@ cf_tab <-
   map(pm, "cf") |> 
   scenario_mean_table()
 
-median_tab <- 
-  map(pm, "median") |> 
-  scenario_mean_table()
-
 combined_tab <- 
   merge(rmst_tab, cf_tab, by = "Scenario") |> 
-  merge(median_tab, by = "Scenario") |> 
   mutate(across(where(is.numeric), ~ round(., 2)))
+
+#############
+# formatting
+
+library(stringr)
 
 # xtable::xtable(combined_tab, digits = 3)
 
-input_table <-
-  scenario_data |> 
-  select(data_id, n_endpoints, nsample, prop_censoring, sigma_true) |>
-  # filter(data_id %in% 1:16) |>
-  kable(format = "latex", booktabs = TRUE)
+##TODO:
+# input_table <-
+#   scenario_data |> 
+#   select(data_id, n_endpoints, nsample, prop_censoring, sigma_true) |>
+#   # filter(data_id %in% 1:16) |>
+#   kable(format = "latex", booktabs = TRUE)
 
 output_table <-
   kable(combined_tab, format = "latex", booktabs = TRUE) |> 
-  add_header_above(c(" ", "RMST" = 4, "Cure Fraction" = 4, "Median" = 4)) 
+  add_header_above(c(" ", "RMST" = 4, "Cure Fraction" = 4)) 
 # kable_styling(latex_options = c("striped", "hold_position"))
+
+output_table_all <-
+  combined_tab_all |> 
+  kable(format = "latex", booktabs = TRUE,
+        col.names = c("Scenario", rep(c("hier", "sep"), 8))) |> 
+  add_header_above(c(" ",
+                     "Bias" = 2, "Coverage" = 2, "empSE" = 2, "RB" = 2,
+                     "Bias" = 2, "Coverage" = 2, "emp SE" = 2, "RB" = 2)) |>
+  add_header_above(c(" ", "RMST" = 8, "Cure Fraction" = 8))
+  
 
 # save
 write.csv(rmst_tab, file = glue::glue("output_data/rmst_table_{model_type}.csv"), row.names = FALSE)
